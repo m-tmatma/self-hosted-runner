c2dfcd9f3e5e9f650651cc6031126c540f39c639
Masaru Tsuchiyama <m.tmatma@gmail.com>
2024-05-12 13:46:18 +0900

    use wget

--- download-runner.sh
+++ download-runner.sh
@@ -2,7 +2,9 @@
 
 mkdir -p actions-runner
 
-# https://github.com/actions/runner/releases/
-gh release -R actions/runner download --skip-existing -p "actions-runner-linux-x64-*.tar.gz" \
-   $(gh release -R actions/runner list --json tagName --jq ".[].tagName" -L 1) \
-   -O actions-runner/actions-runner-linux-x64.tar.gz
+# # https://github.com/actions/runner/releases/
+# gh release -R actions/runner download --skip-existing -p "actions-runner-linux-x64-*.tar.gz" \
+#    $(gh release -R actions/runner list --json tagName --jq ".[].tagName" -L 1) \
+#    -O actions-runner/actions-runner-linux-x64.tar.gz
+
+wget https://github.com/actions/runner/releases/download/v2.316.1/actions-runner-linux-x64-2.316.1.tar.gz -O actions-runner/actions-runner-linux-x64.tar.gz
